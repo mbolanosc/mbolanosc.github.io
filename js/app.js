d3dac493870bd537780fc9dec044d7aaaa8ab272
mbolanosc <mrybc96@gmail.com>
2017-04-11 10:03:52 -0600

    AI with groups

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 //http://scrollmagic.io/docs/index.html
 window.addEventListener('load', init, false);
 function init() {
+    var timing_1 = new TimelineMax();
     var controller_Scroller= new ScrollMagic.Controller();
     var trigger1 = document.getElementById("trigger1");
     var animate1 = document.getElementById("animate1");
@@ -10,10 +11,7 @@ function init() {
     scene1.addTo(controller_Scroller);
 
 		//Second scene. CLOUDS
-    var timeline_1 = new TimelineMax({delay:10});
     function clouds() {
-      //cada id meterlo en un array y con un for
-      //recorrer cada uno de los obj, y incrementarle el tween 3 #.
       var clouds = [];
       clouds.push(document.getElementById("cloud_1"));
       clouds.push(document.getElementById("cloud_2"));
@@ -23,24 +21,37 @@ function init() {
       clouds.push(document.getElementById("cloud_6"));
       clouds.push(document.getElementById("cloud_7"));
       var count_Clouds = 0;
+
       for (var i = 0; i < clouds.length; i++) {
         count_Clouds++;
-        //console.log(clouds[i]);
         var cloudTween = TweenLite.from(clouds[i],count_Clouds,{ease:Back.easeOut.config(1.5), y: "100%"});
       }
       console.log('CONTADOR DE NUBES', count_Clouds);
+      /*var cloudKeepMoving = new TimelineMax({repeat:-1});
+      function floating_Clouds(){
+        var cloudTween = TweenLite.from(clouds,count_Clouds,{ease:Back.easeOut.config(1.5), y: "100%"});
+      }*/
+      //cloudKeepMoving.add(floating_Clouds);
     }
-    
-		//Animation of popcorn.
-		//hacer una despues de otra.
 
+    //sun.
+	   function sun_moving(){
+     //var sun =
+     document.getElementsByClassName("st46");
+     //console.log(sun);
+     TweenMax.to("st46", 2.5, {scaleX:1, scaleY:0.8, opacity:0.7});
+     }
+
+    //smoke.
+    //clementine eyes.
     var scene2 = new ScrollMagic.Scene({ triggerElement: trigger2 });
     scene2.setTween(clouds); // Start the clouds animation.
+    timing_1.add(sun_moving);
+    //scene2.setTween(sun_moving); // Start the sun animation.
     scene2.addTo(controller_Scroller);
 
-		//smoke.
-		//sun.
-		//clementine eyes.
+
+
 
 }
 
