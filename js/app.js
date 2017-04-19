3d5afd8423a08f5780af1fd9a65547e1f096c89f
mbolanosc <mrybc96@gmail.com>
2017-04-18 23:17:48 -0600

    timeline fix with callbacks, also the floating clouds animation is better, still missing some animations about Clementine legs.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //http://scrollmagic.io/docs/index.html
 window.addEventListener('load', init, false);
 function init() {
-  var tl = new TimelineMax({repeat:-1});
+  var tl = new TimelineMax({repeat: -1});
   let clouds_timeline = new TimelineMax();
   var controller_Scroller = new ScrollMagic.Controller();
 
@@ -19,98 +19,136 @@ function init() {
 
   for (var i = 0; i < clouds.length; i++) {
     count_Clouds++;
-    clouds_timeline.add(TweenLite.from(clouds[i], 1, { ease: Back.easeOut.config(1.5), y: "100%" }));
+    clouds_timeline.add(TweenLite.from(clouds[i], 1, {
+      ease: Back.easeOut.config(1.5),
+      y: "100%"
+    }));
   }
 
   //Add the onComplete callback to the last tween
-  clouds_timeline.add(TweenLite.from(document.getElementById("cloud_7"), 1, { ease:Back.easeOut.config(1.5), y: "100%", onComplete: sunMoving }));
+  clouds_timeline.add(TweenLite.from(document.getElementById("cloud_7"), 1, {
+    ease: Back.easeOut.config(1.5),
+    y: "100%",
+    onComplete: generalTimeLine
+  }));
 
-  /*function clouds_Floating(){
-    var keep_floating = new TimelineMax({repeat:-1, yoyo:true});
-
-    var clouds = [];
-    clouds.push(document.getElementById("cloud_1"));
-    clouds.push(document.getElementById("cloud_2"));
-    clouds.push(document.getElementById("cloud_3"));
-    clouds.push(document.getElementById("cloud_4"));
-    clouds.push(document.getElementById("cloud_5"));
-    clouds.push(document.getElementById("cloud_6"));
-    clouds.push(document.getElementById("cloud_7"));
-    clouds.push(document.getElementById("cloud_8"));
-
-    for (var i = 0; i < clouds.length; i++) {
-      console.log("float");
-      keep_floating.add(TweenLite.from(clouds[i], 1, { ease: Back.easeOut.config(1.5), y: "5%" }));
-    }
-  }
-  */
-
-  function sunMoving() {
-    console.log('sun_moving()');
-    var sun_timeline = new TimelineMax({onComplete:blinkingEyes,repeat:-1, yoyo:true});
+  function generalTimeLine() {
+    console.log('generalTimeLine');
+    var general_tm = new TimelineMax();
+    var sun_timeline = new TimelineMax({onComplete: blinkingEyes, repeat: -1, yoyo: true});
     //outerline
-    sun_timeline.fromTo('#sun',1,{scale:1,transformOrigin:"0% 100%"},{scale:1.2,ease:Power2.easeOut});
+    sun_timeline.fromTo('#sun', 1, {
+      scale: 1,
+      transformOrigin: "0% 100%"
+    }, {
+      scale: 1.2,
+      ease: Power2.easeOut
+    });
+    function clouds_Floating() {
+      console.log('clouds_Floating');
+      var keep_floating = new TimelineMax({repeat: -1, yoyo: true});
 
-    //callback
+      var clouds = [];
+      clouds.push(document.getElementById("cloud_1"));
+      clouds.push(document.getElementById("cloud_2"));
+      clouds.push(document.getElementById("cloud_3"));
+      clouds.push(document.getElementById("cloud_4"));
+      clouds.push(document.getElementById("cloud_5"));
+      clouds.push(document.getElementById("cloud_6"));
+      clouds.push(document.getElementById("cloud_7"));
+      clouds.push(document.getElementById("cloud_8"));
 
-  }
+      var elementAnimationDelay = 0;
+      var countMoveUp = 0;
+      var countMoveDown = 0;
 
-  function blinkingEyes() {
-    console.log('blinkingEyes()');
+      function elementMoveUp(elementItem) {
+        TweenMax.to(elementItem, 2, {top: "-=10px", ease: Power2.easeIn, onCompleteParams: [elementItem], onComplete: elementMoveDown});
+      }
+      function elementMoveDown(elementItem) {
+        console.log('element down');
+        TweenMax.to(elementItem, 2, {delay: 2,y: "-=10px",ease: Power2.easeOut,repeat: -1,
+          yoyo: true,onCompleteParams: [elementItem],onComplete: elementMoveUp});
+      }
 
-    var eye1_timeline = new TimelineMax({repeat:-1, yoyo:true});
-    var eye2_timeline = new TimelineMax({repeat:-1, yoyo:true});
-
-    eye1_timeline.to('#eye_1',2,{fill:"#0000"});
-    eye2_timeline.to('#eye_2',2,{fill:"#0000"});
-
-    //callback
-  }
-
-  function smoke(){
-    console.log('smoke()');
-    var smoke_timeLine = new TimelineMax({repeat:-1, yoyo:true});
-    var smoke_clouds = [];
-    smoke_clouds.push(document.getElementById("smoke_1"));
-    smoke_clouds.push(document.getElementById("smoke_2"));
-    smoke_clouds.push(document.getElementById("smoke_3"));
-    smoke_clouds.push(document.getElementById("smoke_4"));
-
-    for (var i = 0; i < smoke_clouds.length; i++) {
-      smoke_timeLine.add(TweenLite.from(smoke_clouds[i], 0.5, {opacity:0.2,ease:Power2.easeInOut}));
+      if (clouds == null) {
+        console.log('esta nulo');
+      }else{
+        console.log('no esta nulo');
+        for (var i = 0; i < clouds.length; i++) {
+          //console.log('sds',clouds[i]);
+          countMoveUp++;
+          countMoveDown++;
+          elementMoveDown(clouds[i]);
+        }
+      }
     }
 
-    //callback
+    function smoke() {
+      console.log('smoke()');
+      var smoke_timeLine = new TimelineMax({repeat: -1, yoyo: true});
+      var smoke_clouds = [];
+      smoke_clouds.push(document.getElementById("smoke_1"));
+      smoke_clouds.push(document.getElementById("smoke_2"));
+      smoke_clouds.push(document.getElementById("smoke_3"));
+      smoke_clouds.push(document.getElementById("smoke_4"));
+
+      for (var i = 0; i < smoke_clouds.length; i++) {
+        smoke_timeLine.add(TweenLite.from(smoke_clouds[i], 0.5, {
+          opacity: 0.2,
+          ease: Power2.easeInOut
+        }));
+      }
+
+    }
+
+    function blinkingEyes() {
+      console.log('blinkingEyes()');
+
+      var eye1_timeline = new TimelineMax({repeat: -1, yoyo: true});
+      var eye2_timeline = new TimelineMax({repeat: -1, yoyo: true});
+
+      eye1_timeline.to('#eye_1', 2, {fill: "#0000"});
+      eye2_timeline.to('#eye_2', 2, {fill: "#0000"});
+    }
+
+    function clementine_first_look() {
+      console.log('clementine 1');
+      var clementine_first_appear = new TimelineMax();
+
+      clementine_first_appear.fromTo('#clementine-first', 0.4, {
+        opacity: 0
+      }, {
+        opacity: 3,
+        ease: Power1.easeInOut,
+        display: 'block',
+        delay: 5
+      })
+
+    }
+    function clementine_moves() {
+      console.log('clementine eyes');
+      //eyes PARPADEO MEJORAR.
+      var clementine_eyes_timeline = new TimelineMax({repeat: -1, yoyo: true});
+      clementine_eyes_timeline.to('.clementine-eyes', 2, {fill: "#FFFFFF"});
+      //callback
+
+    }
+
+    general_tm.add(clouds_Floating);
+    general_tm.add(smoke);
+    general_tm.add(blinkingEyes);
+    general_tm.add(clementine_first_look);
+    general_tm.add(clementine_moves);
 
   }
 
-  function clementine_first_look (){
-    console.log('clementine 1');
-    var clementine_first_appear = new TimelineMax();
-
-    clementine_first_appear.fromTo('#clementine-first', 0.4, {opacity:0}, {opacity:3, ease:Power1.easeInOut,display:'block'})
-
-    //callback
-
-  }
-
-  function clementine_moves() {
-    console.log('clementine eyes');
-    //eyes PARPADEO MEJORAR.
-    var clementine_eyes_timeline= new TimelineMax({repeat:-1, yoyo:true});
-    clementine_eyes_timeline.to('.clementine-eyes',2,{fill:"#FFFFFF"});
-    //callback
-
-  }
-
-  
-  var scene1 = new ScrollMagic.Scene({ triggerElement: trigger2 });
+  //Scroll things.
+  var scene1 = new ScrollMagic.Scene({triggerElement: trigger2});
   scene1.setTween(clouds_timeline); // Start the clouds animation.
-  scene1.addIndicators({ name: "2 (duration: 300 px)" });
+  scene1.addIndicators({name: "2 (duration: 300 px)"});
   scene1.addTo(controller_Scroller);
 }
-
-
 
 /*orden
 1. nubes YA
@@ -126,6 +164,10 @@ function init() {
 11. clementine se va a dormir. NO
 */
 
-
 //FALTAS:
-//problemas con los callback
+//4. el viento de la cubeta de agua.
+//8. clementine camina hacia la cubeta de agua. NO
+//9. clementine se moja con el agua.NO
+//10. clementine camina hacia las manzanas. NO
+//11. clementine se va a dormir. NO
+//12 hacer caminito de piedra.
