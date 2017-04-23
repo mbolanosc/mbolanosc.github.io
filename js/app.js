946b4720a40d247a6702f27d80b2717a175a5a1a
mbolanosc <mrybc96@gmail.com>
2017-04-23 00:07:46 -0600

    clementine walks to the tree, the water is almost down, and clementine is now scare when the watergets down, still troubles with the water animation, transitions, and timing.

--- js/app.js
+++ js/app.js
@@ -33,7 +33,6 @@ function init() {
   }));
 
   function generalTimeLine() {
-    console.log('generalTimeLine');
     var general_tm = new TimelineMax();
     var sun_timeline = new TimelineMax({onComplete: blinkingEyes, repeat: -1, yoyo: true});
     //outerline
@@ -45,7 +44,6 @@ function init() {
       ease: Power2.easeOut
     });
     function clouds_Floating() {
-      console.log('clouds_Floating');
       var keep_floating = new TimelineMax({repeat: -1, yoyo: true});
 
       var clouds = [];
@@ -58,23 +56,31 @@ function init() {
       clouds.push(document.getElementById("cloud_7"));
       clouds.push(document.getElementById("cloud_8"));
 
-      var elementAnimationDelay = 0;
+      //  var elementAnimationDelay = 0;
       var countMoveUp = 0;
       var countMoveDown = 0;
 
       function elementMoveUp(elementItem) {
-        TweenMax.to(elementItem, 2, {top: "-=10px", ease: Power2.easeIn, onCompleteParams: [elementItem], onComplete: elementMoveDown});
+        TweenMax.to(elementItem, 2, {
+          top: "-=10px",
+          ease: Power2.easeIn,
+          onCompleteParams: [elementItem],
+          onComplete: elementMoveDown
+        });
       }
       function elementMoveDown(elementItem) {
-        console.log('element down');
-        TweenMax.to(elementItem, 2, {delay: 2,y: "-=10px",ease: Power2.easeOut,repeat: -1,
-          yoyo: true,onCompleteParams: [elementItem],onComplete: elementMoveUp});
+        TweenMax.to(elementItem, 2, {
+          delay: 2,
+          y: "-=10px",
+          ease: Power2.easeOut,
+          repeat: -1,
+          yoyo: true,
+          onCompleteParams: [elementItem],
+          onComplete: elementMoveUp
+        });
       }
 
-      if (clouds == null) {
-        console.log('esta nulo');
-      }else{
-        console.log('no esta nulo');
+      if (clouds == null) {} else {
         for (var i = 0; i < clouds.length; i++) {
           //console.log('sds',clouds[i]);
           countMoveUp++;
@@ -85,7 +91,6 @@ function init() {
     }
 
     function smoke() {
-      console.log('smoke()');
       var smoke_timeLine = new TimelineMax({repeat: -1, yoyo: true});
       var smoke_clouds = [];
       smoke_clouds.push(document.getElementById("smoke_1"));
@@ -103,16 +108,15 @@ function init() {
     }
 
     function blinkingEyes() {
-      console.log('blinkingEyes()');
       //cambiarlo por tween y agregarle un tween al timeline
-      var eye1_timeline = new TimelineMax({onComplete: notBlinkingTree,repeat: -1, yoyo: true});
-      var eye2_timeline = new TimelineMax({onComplete: notBlinkingTree,repeat: -1, yoyo: true});
+      var eye1_timeline = new TimelineMax({onComplete: notBlinkingTree, repeat: -1, yoyo: true});
+      var eye2_timeline = new TimelineMax({onComplete: notBlinkingTree, repeat: -1, yoyo: true});
 
       eye1_timeline.to('#eye_1', 2, {fill: "#0000"});
       eye2_timeline.to('#eye_2', 2, {fill: "#0000"});
     }
     //sigue mala es para dejar de parpadear.
-    function notBlinkingTree(){
+    function notBlinkingTree() {
       console.log('not blinking');
       var not_blinking_tm = new TimelineMax();
       not_blinking_tm.to('#eye_1', 2, {fill: "#0000"});
@@ -132,34 +136,89 @@ function init() {
         ease: Power1.easeInOut,
         display: 'block',
         delay: 8
-      })
+      });
 
     }
     function clementine_moves() {
+      var clementinePathTreeTm = new TimelineMax();
+
       console.log('clementine eyes');
-      //eyes PARPADEO MEJORAR.
-      var clementine_eyes_timeline = new TimelineMax({repeat: -1, yoyo: true});
-      clementine_eyes_timeline.to('.clementine-eyes', 2, {fill: "#FFFFFF"});
-      //mover a clementine hasta el agua
-      console.log('move clementine');
+      var clementine_eyes_timeline = TweenMax.to('.clementine-eyes', 2.5, {
+        repeat: -1,
+        yoyo: true,
+        scaleY: 0.3,
+        scaleX: 1.7
+      });
+      //mover a clementine hasta el arbol
+      move_1 = TweenMax.to('#clementine-first', 2, {
+        x: "-=70%",
+        delay: 10
+      });
+      move_2 = TweenMax.to('#clementine-first', 2, {
+        y: "+70%",
+        delay: 12
+      });
 
-      var clementine_to_water = new TimelineMax();
-      clementine_to_water.to('#clementine-first', 1,{x:"-=150%",y:"=100%",delay:12});
-      //patitas
-
-      //path: tweens para un path a seguir agregar el callback
+      clementinePathTreeTm.add(TweenLite.to(document.getElementById("clementine-first"), 2, {
+        onComplete: clementineStandUp,
+        x: "-=90%",
+        y: "=95%",
+        delay: 14
+      }));
     }
-    function water(){
+
+    function clementineStandUp() {
+      var clementineStandsUpTm = new TimelineMax();
+
+      console.log('clementineStandUp');
+      //esconder el anterior y aparecer el de pie.
+      var clementineNotDisplay = TweenMax.to('#clementine-first', 1, {
+        display: 'none',
+        ease: Power1.easeInOut,
+        delay: 1
+      });
+
+    clementineStandsUpTm.add(TweenLite.to(document.getElementById("clementine-stand_up"), 1, {
+        onComplete: water,
+        display: 'block',
+        ease: Power1.easeInOut,
+        delay: 2
+      }));
+    }
+    //hacer que el agua baje
+    function water() {
+      var WaterMovesDown = new TimelineMax();
+
       console.log('water');
-      TweenLite.to(".st58", 1.5, {width:100, height:200});
+      TweenLite.to(".st58", 1.5, {scaleY: 2.5});
+      //TweenLite.to("#balde", 1.5, {scaleY:2.5});
+      WaterMovesDown.add(TweenLite.to(document.getElementById("balde"), 1.5, {
+        onComplete: clementineScare,
+        scaleY: 2.5
+      }));
 
+      function clementineScare(){
+        var clementineScareShows = new TimelineMax();
+
+        var clementineNotDisplay = TweenMax.to('#clementine-stand_up', 1, {
+          display: 'none',
+          ease: Power1.easeInOut,
+          delay: 1
+        });
+        clementineScareShows.add(TweenLite.to(document.getElementById("clementine-scare"), 1, {
+          //  onComplete: water,
+            display: 'block',
+            ease: Power1.easeInOut,
+            delay: 2
+          }));
+      }
     }
+
     general_tm.add(clouds_Floating);
     general_tm.add(smoke);
     general_tm.add(blinkingEyes);
     general_tm.add(clementine_first_look);
     general_tm.add(clementine_moves);
-    general_tm.add(water);
 
   }
 
